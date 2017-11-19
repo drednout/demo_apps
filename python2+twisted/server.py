4ae2dec8faf84467aa14eccff7e838d0f796f92d
Aleksey Romanov <a_romanov@wargaming.net>
2017-11-19 19:17:28 +0300

    Added demo services for  python3+aiohttp and python2+twisted+cyclone

---
+++ python2+twisted/server.py
@@ -0,0 +1,62 @@
+import sys
+import logging
+
+from twisted.internet import reactor, defer
+from twisted.web.client import getPage
+from twisted.python import log
+import cyclone.web
+
+def sleep(n):
+    '''Sleep n seconds inside reactor without blocking other co-routines.
+    :param n: float
+    :return: Deferred()
+    '''
+    d = defer.Deferred()
+    reactor.callLater(n, d.callback, True)
+    return d
+
+class RootHandler(cyclone.web.RequestHandler):
+    def get(self):
+        text = 'Hello from python2.7+twisted'
+        self.write(text)
+
+class CpuLoad(cyclone.web.RequestHandler):
+    def get(self):
+        min_value = int(self.get_argument('min', 1))
+        max_value = int(self.get_argument('max', 1000))
+        s = sum(range(min_value, max_value + 1))
+        text = 'sum({}..{})={}'.format(min_value, max_value, s)
+        self.write(text)
+
+
+class SlowResp(cyclone.web.RequestHandler):
+    @defer.inlineCallbacks
+    def get(self):
+        timeout = float(self.get_argument('timeout', 1.0))
+        yield sleep(timeout)
+        text = 'Slow hello from python2.7+twisted'
+        self.write(text)
+
+class Gateway(cyclone.web.RequestHandler):
+    @defer.inlineCallbacks
+    def get(self):
+        url = str(self.get_argument('url', 'http://localhost'))
+        resp = yield getPage(url, method='GET')
+        self.write(resp)
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s: %(message)s')
+    logging.root.setLevel(logging.ERROR)
+
+    application = cyclone.web.Application([
+        (r'/', RootHandler),
+        (r'/cpu_load', CpuLoad),
+        (r'/slow_resp', SlowResp),
+        (r'/gateway', Gateway),
+    ])
+
+    observer = log.PythonLoggingObserver()
+    observer.start()
+
+    reactor.listenTCP(2700, application)
+    reactor.run()
---
+++ python3+aiohttp/server.py
@@ -0,0 +1,40 @@
+import asyncio
+import aiohttp
+from aiohttp import web
+import async_timeout
+
+async def handle_root(request):
+    text = "Hello from python3.6+aiohttp"
+    return web.Response(text=text)
+
+async def handle_cpu_load(request):
+    min_value = int(request.query.get('min', 1))
+    max_value = int(request.query.get('max', 1000))
+    s = sum(range(min_value, max_value + 1))
+    text = "sum({}..{})={}".format(min_value, max_value, s)
+    return web.Response(text=text)
+
+async def handle_slow_resp(request):
+    timeout = float(request.query.get('timeout', 1.0))
+    await asyncio.sleep(timeout)
+    text = "Slow hello from python3.6+aiohttp"
+    return web.Response(text=text)
+
+async def fetch(session, url):
+    with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            return await response.text()
+
+async def handle_gateway(request):
+    url = request.query.get('url', 'http://localhost')
+    async with aiohttp.ClientSession() as session:
+        resp = await fetch(session, url)
+        return web.Response(text=resp)
+
+app = web.Application()
+app.router.add_get('/', handle_root)
+app.router.add_get('/cpu_load', handle_cpu_load)
+app.router.add_get('/slow_resp', handle_slow_resp)
+app.router.add_get('/gateway', handle_gateway)
+
+web.run_app(app, port=3600)
